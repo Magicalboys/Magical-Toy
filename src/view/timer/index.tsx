32c60144da4822ce7cc24b025340491578e7489b
spellbind <2591754854@qq.com>
2023-07-17 22:46:05 +0800

    feat:add timer

---
+++ src/view/timer/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useRef, useState} from 'react';
+
+export const Timer = () => {
+  const [time, setTime] = useState(0);
+  const timer = useRef<any>();
+  const stop = () => {
+    clearInterval(timer.current);
+  };
+
+  const start = () => {
+    if (time <= 60) {
+      timer.current = setInterval(() => {
+        setTime((time) => time + 1);
+      }, 1000);
+    }
+  };
+
+  return (
+    <div className="App">
+      <button onClick={()=>start()}>开始</button>
+      <button onClick={()=>stop()}>暂停</button>
+      <div>
+        {
+          time
+        }
+      </div>
+    </div>
+  );
+};
